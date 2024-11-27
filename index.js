f99159ec5152288acf76d8f2641f796842f6a655
Supreme Raut <supremeraut94@gmail.com>
2024-11-28 00:41:47 +0545

    added the remaining methods

--- index.js
+++ index.js
@@ -41,16 +41,89 @@ class HashMap {
   get(key) {
     let index = this.hash(key) % this.buckets.length;
     const bucket = this.buckets[index];
-    for (let pair of bucket) {
-      if (pair[0] === key) {
-        return pair[1];
-      } else {
-        return undefined;
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return bucket[i][1];
       }
     }
+    return undefined;
+  }
+  has(key) {
+    let index = this.hash(key) % this.buckets.length;
+    const bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(key) {
+    let index = this.hash(key) % this.buckets.length;
+    const bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        this.items--;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  length() {
+    return this.items;
+  }
+  clear() {
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    this.items = 0;
+  }
+  keys() {
+    const keysArr = [];
+    this.buckets.forEach((bucket) => {
+      for (let i = 0; i < bucket.length; i++) {
+        keysArr.push(bucket[i][0]);
+      }
+    });
+
+    return keysArr;
+  }
+
+  values() {
+    const valuesArr = [];
+    this.buckets.forEach((bucket) => {
+      for (let i = 0; i < bucket.length; i++) {
+        valuesArr.push(bucket[i][1]);
+      }
+    });
+
+    return valuesArr;
+  }
+  entries() {
+    const entryArr = [];
+    this.buckets.forEach((bucket) => {
+      for (let i = 0; i < bucket.length; i++) {
+        entryArr.push(bucket[i]);
+      }
+    });
+
+    return entryArr;
   }
 }
-let a = new HashMap();
-a.set("banana", "green");
-console.log(a.get("kjs"));
-//console.log(a.buckets);
+const test = new HashMap(); // or HashMap() if using a factory
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+test.set("moon", "silver");
+
+console.log(test.buckets);
